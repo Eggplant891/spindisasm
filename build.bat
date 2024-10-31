5283e800aea44dab9fcc0db64d6620639c643164
Jason Harmon <jharmon891@yahoo.co.uk>
2024-10-31 22:27:05 +0000

    Update build batch files to output to bin folder

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 @echo off
 
-IF EXIST sbbuilt.bin move /Y sbbuilt.bin sbbuilt.prev.bin >NUL
-asm68k /k /p /o ae- spinball.asm, sbbuilt.bin >errors.txt, , spinball.lst
-rem copy /Y sbbuilt.bin sbbuilt_exodus.bin
+IF EXIST bin\sbbuilt.bin move /Y bin\sbbuilt.bin bin\sbbuilt.prev.bin >NUL
+asm68k /k /p /o ae- spinball.asm, bin\sbbuilt.bin >errors.txt, , spinball.lst
--- build_modified.bat
+++ build_modified.bat
@@ -1,4 +1,4 @@
 @echo off
 
-IF EXIST sbbuilt_modified.bin move /Y sbbuilt_modified.bin sbbuilt_modified.prev.bin >NUL
-asm68k /k /p /o ae- spinball_modified.asm, sbbuilt_modified.bin >errors_modified.txt, , spinball_modified.lst
+IF EXIST bin\sbbuilt_modified.bin move /Y bin\sbbuilt_modified.bin bin\sbbuilt_modified.prev.bin >NUL
+asm68k /k /p /o ae- spinball_modified.asm, bin\sbbuilt_modified.bin >errors_modified.txt, , spinball_modified.lst

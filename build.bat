bfd2a727360d8c08737e7739a4985a08ffc6b75a
Jason Harmon <jharmon891@yahoo.co.uk>
2024-11-30 10:22:34 +0000

    Improved build batch script that allows dragging & dropping the asm file onto it - Will also now print the errors.txt in the console

--- build.bat
+++ build.bat
@@ -1,4 +1,14 @@
 @echo off
 
-IF EXIST bin\sbbuilt.bin move /Y bin\sbbuilt.bin bin\sbbuilt.prev.bin >NUL
-asm68k /k /p /o ae- spinball.asm, bin\sbbuilt.bin >errors.txt, , spinball.lst
+SET project_name=%~1
+echo %project_name%
+for %%f in ("%project_name%") do set project_name=%%~nf
+
+if not exist "output\" mkdir output
+
+if exist bin\%project_name%.bin move /Y bin\%project_name%.bin bin\%project_name%.prev.bin >NUL
+asm68k /k /p /o ae- %project_name%.asm, bin\%project_name%.bin >output\errors.txt, , output\%project_name%.lst
+
+type output\errors.txt
+echo: 
+@pause
